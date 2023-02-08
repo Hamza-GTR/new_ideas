eb53f3edc34b389e2cd7d130dfe9378b1761f176
Hamza <hmztabba@gmail.com>
2023-02-08 16:29:11 +0200

    adding second table and his relation.

--- challenge1/app1/models.py
+++ challenge1/app1/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
+
 
 # Create your models here.
 
@@ -9,3 +11,10 @@ class Covers(models.Model):
     description = models.CharField(max_length=100)
     price = models.PositiveIntegerField()
     discount = models.IntegerField()
+
+
+class customer(models.Model):
+    name = models.CharField(max_length=50)
+    relation = models.ForeignKey(User, on_delete=models.CASCADE, default=None, blank=False)
+
+
