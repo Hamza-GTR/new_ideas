2ffb556cae4ac09bf35e33597791130b1f0b6098
Hamza <hmztabba@gmail.com>
2023-02-09 19:43:20 +0200

    new model named Details with a ForeignKey to the Covers table.

--- challenge1/app1/models.py
+++ challenge1/app1/models.py
@@ -8,13 +8,15 @@ from django.db import models
 class Covers(models.Model):
     name = models.CharField(max_length=50)
     type = models.CharField(max_length=50)
-    description = models.CharField(max_length=100)
-    price = models.PositiveIntegerField()
-    discount = models.IntegerField()
 
 
-class customer(models.Model):
+class Customer(models.Model):
     name = models.CharField(max_length=50)
     relation = models.ForeignKey(User, on_delete=models.CASCADE, default=None, blank=False)
 
 
+class Details(models.Model):
+    covers = models.ForeignKey(Covers, on_delete=models.CASCADE, default=None)
+    description = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+    discount = models.IntegerField()
