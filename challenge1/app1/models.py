1a401d96922afc7c05329b79fb31bda02d5f5665
Hamza <hmztabba@gmail.com>
2023-02-17 02:43:20 +0200

    adding Order table.

--- challenge1/app1/models.py
+++ challenge1/app1/models.py
@@ -20,7 +20,11 @@ class Customer(models.Model):
     address = models.TextField()
 
 
-
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    total_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    shipping_address = models.TextField()
 
 
 class ProductDetails(models.Model):
