e904f10001e572144530e49f5957f6ebe3f8890c
Hamza <hmztabba@gmail.com>
2023-02-17 02:41:31 +0200

    removing Covers table.

--- challenge1/app1/models.py
+++ challenge1/app1/models.py
@@ -5,12 +5,7 @@ from django.db import models
 # Create your models here.
 
 
-class Covers(models.Model):
-    name = models.CharField(max_length=50)
-    type = models.CharField(max_length=50)
-    description = models.CharField(max_length=200)
-    price = models.PositiveIntegerField()
-    quantity = models.PositiveIntegerField()
+
 
 
 class Customer(models.Model):
