284f023694c9c56c14e711f769abee7b8458c9f5
Hamza <hmztabba@gmail.com>
2023-02-18 23:21:47 +0200

    editing Covers table.

--- challenge1/app1/models.py
+++ challenge1/app1/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 
 class Covers(models.Model):
-    
+    name = models.CharField(max_length=50)
     type = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     price = models.PositiveIntegerField()
