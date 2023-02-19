82baa4862e1ec52e4eee12703293b6b3497afa0b
Hamza <hmztabba@gmail.com>
2023-02-19 12:29:42 +0200

    editing relations between tables.

--- challenge1/app1/models.py
+++ challenge1/app1/models.py
@@ -21,7 +21,7 @@ class Customer(models.Model):
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer.first_name, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
     total_cost = models.DecimalField(max_digits=10, decimal_places=2)
     shipping_address = models.TextField()
@@ -41,6 +41,6 @@ class Products(models.Model):
 
 
 class ShoppingCart(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer.first_name, on_delete=models.CASCADE)
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
